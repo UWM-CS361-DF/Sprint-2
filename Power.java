dfa0aac5371f287abc804cd27f76bfb4d0c24bfb
kkrowsky <krowsky@uwm.edu>
2017-03-12 10:18:09 -0500

    don't need

--- Power.java
+++ Power.java
@@ -4,11 +4,6 @@ public class Power {
 	boolean powerStatus=false;
 	
 	public boolean power(){
-		for(int i=1;i<9;i++)
-			ChronoInterface.chronoTimer.channels.get(i).isArmed=false;//disarm channels when poweroff
-		ChronoInterface.chronoTimer.race.startQueue.clear();//reset race
-		ChronoInterface.chronoTimer.race.finishQueue.clear();//reset race
-		ChronoInterface.chronoTimer.race.completed.clear();//reset race
 		powerStatus=!powerStatus;//switch on when poweroff and vice versa
 		return powerStatus;
 	}
