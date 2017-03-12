1057e4deaf1ae6797038cd7a5a7c1fc7bac290b4
kkrowsky <krowsky@uwm.edu>
2017-03-12 10:02:05 -0500

    Starting from Sprint 1

--- Power.java
+++ Power.java
@@ -4,7 +4,7 @@ public class Power {
 	boolean powerStatus=false;
 	
 	public boolean power(){
-		for(int i=0;i<9;i++)
+		for(int i=1;i<9;i++)
 			ChronoInterface.chronoTimer.channels.get(i).isArmed=false;//disarm channels when poweroff
 		ChronoInterface.chronoTimer.race.startQueue.clear();//reset race
 		ChronoInterface.chronoTimer.race.finishQueue.clear();//reset race
